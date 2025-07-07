f59ba00c5deea8aa0c4d647b770bf111e47a1c3e
Janell-Huyck <huyckjl@ucmail.uc.edu>
2025-07-07 17:34:28 -0400

    Program 7 complete

--- CtoFTableDisplay.java
+++ CtoFTableDisplay.java
@@ -1,10 +1,18 @@
 public class CtoFTableDisplay {
     public static void main(String[] args) {
+        double fahrenheit;
 
+        System.out.printf("%8s | %10s%n", "Celsius", "Fahrenheit");
+        System.out.println("---------+------------");
+
+        for (int celsius = -100; celsius <= 100; celsius++) {
+            fahrenheit = CtoF(celsius);
+            System.out.printf("%8d │ %12.2f%n", celsius, fahrenheit);
+        }
     }
 
-    public static double CtoF(double Celsius) {
-        return 3.5;
+    public static double CtoF(double celsius) {
+        return celsius * 9.0 / 5.0 + 32.0;
     }
 
 }
